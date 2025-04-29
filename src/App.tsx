af4fe36f9a03e0afd36b20d501b61c93b60983e8
sparkweaver <lundmark.johan.91@gmail.com>
2025-04-29 16:16:16 +0200

    fix: ensure that footer is at the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Theme from "./theme"
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Theme from "./theme";
 
 const App: React.FC = () => {
-    return (
-        <ThemeProvider theme={Theme}>
-                <Router>
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/projects" element={<Projects />} />
-                        <Route path="/contact" element={<Contact />} />
-                    </Routes>
-                    <Footer />
-                </Router>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={Theme}>
+      <Router>
+        <main style={{flex: 1}}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </ThemeProvider>
+  );
 };
 
 export default App;
