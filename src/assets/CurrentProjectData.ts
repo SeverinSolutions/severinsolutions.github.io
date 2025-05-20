92c96a7f4ad7103c2b231b172b20722ee7546b67
sparkweaver <lundmark.johan.91@gmail.com>
2025-05-20 12:31:25 +0200

    add: new project that I am working on.

--- src/assets/CurrentProjectData.ts
+++ src/assets/CurrentProjectData.ts
@@ -6,10 +6,22 @@ const CurrentProjectData = [
       "Developing a comprehensive system to efficiently manage project time tracking and worker allocation.",
     problem: "Currently under development",
     skills: ["ASP.NET"],
-    githubLink:
-      "",
+    githubLink: "",
     githubReadme:
       "https://raw.githubusercontent.com/SeverinSolutions/documentation/main/nobergergrav-readme.md",
+    youtubeLink: "",
+  },
+  {
+    title: "Jobhunter Management Application",
+    image: "/images/jobhunter.PNG",
+    description:
+      "An application for managing and tracking job applications, providing insights and statistics to optimize the job search process.",
+    problem:
+      "The original goal was to automate job tracking and analytics using AI, but budget constraints currently limit the project to manual input and basic statistical tracking.",
+    skills: ["Vite", "React", "MUI", "TypeScript", "Python", "SQL"],
+    githubLink: "",
+    githubReadme:
+      "https://raw.githubusercontent.com/SeverinSolutions/documentation/main/jobhunter-readme.md",
     youtubeLink: "",
   },
 ];
