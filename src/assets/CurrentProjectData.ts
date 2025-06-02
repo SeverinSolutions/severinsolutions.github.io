5d1c2a08654e113212faaa5009b242bf5c1adc7a
sparkweaver <lundmark.johan.91@gmail.com>
2025-06-02 23:32:52 +0200

    update: current project skills

--- src/assets/CurrentProjectData.ts
+++ src/assets/CurrentProjectData.ts
@@ -6,7 +6,7 @@ const CurrentProjectData = [
       "An application for managing and tracking job applications, providing insights and statistics to optimize the job search process.",
     problem:
       "The original goal was to automate job tracking and analytics using AI, but budget constraints currently limit the project to manual input and basic statistical tracking.",
-    skills: ["Vite", "React", "MUI", "TypeScript", "Python", "SQL"],
+    skills: ["Vite", "React", "Tailwind", "PostCSS","TypeScript", "HeadlessUI", "ASP.NET", "SQL"],
     githubLink: "https://github.com/SeverinSolutions/jobhunter",
     githubReadme:
       "https://raw.githubusercontent.com/SeverinSolutions/documentation/main/jobhunter-readme.md",
