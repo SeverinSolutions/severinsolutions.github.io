23ba188a9a53b24ea15da0a06485f794e129fc76
sparkweaver <lundmark.johan.91@gmail.com>
2025-06-18 23:26:03 +0200

    add: my largest project from my education

--- src/assets/ArchiveProjectData.ts
+++ src/assets/ArchiveProjectData.ts
@@ -4,12 +4,27 @@ const ArchiveProjectData = [
     image: "/images/ss-webpage.png",
     description:
       "A clean, responsive portfolio site showcasing my IT background, projects, and contact information.",
-    problem: "Deploying a React app on GitHub Pages posed challenges with routing and fetching dokuments.",
+    problem:
+      "Deploying a React app on GitHub Pages posed challenges with routing and fetching dokuments.",
     skills: ["Vite", "React", "TypeScript", "MUI"],
     githubLink:
       "https://github.com/SeverinSolutions/severinsolutions.github.io",
     githubReadme:
       "https://raw.githubusercontent.com/SeverinSolutions/documentation/main/severinsolutions-readme.md",
+    youtubeLink: "",
+  },
+  {
+    title: "GitLab Issues Management Platform",
+    image: "/images/gitlab.png",
+    description:
+      "A comprehensive web application developed during my studies, enabling users to manage GitLab issues in real-time with an intuitive interface.",
+    problem:
+      "Working with the GitLab REST API and Webhooks posed limitations. To address this, I implemented an 'illusion layer' a custom abstraction that enhances the user experience by masking API shortcomings.",
+    skills: ["JavaScript", "EJS", "CSS"],
+    githubLink:
+      "https://github.com/SeverinSolutions/gitlab-repo-issue-manager-assignment",
+    githubReadme:
+      "https://raw.githubusercontent.com/SeverinSolutions/documentation/main/gitlabmanagement-readme.md",
     youtubeLink: "",
   },
 ];
